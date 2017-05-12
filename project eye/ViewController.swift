75763143e9abd5cfa031de0280244ef51cf09f65
陳鈞廷 <hankchen@Hanks-MacBook-Pro.local>
2017-05-13 03:14:11 +0800

    Initial Commit

---
+++ project eye/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project eye
+//
+//  Created by 陳鈞廷 on 2017/5/13.
+//  Copyright © 2017年 陳鈞廷. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
